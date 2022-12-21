bf69e7723789593589ecc0a229ea2edcae6911f4
Sean Li <sean.4736zg@gmail.com>
2022-12-21 16:23:56 +0200

    Add  CircularProgressTimerController

--- Sources/CircularProgressTimer/CircularProgressTimer.swift
+++ Sources/CircularProgressTimer/CircularProgressTimer.swift
@@ -3,12 +3,12 @@
 //
 
 import SwiftUI
+import Combine
 
 @available(iOS 13.0.0, macOS 10.15, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 public struct CircularProgressTimer: View {
-  private let timer = Timer
-    .publish(every: 1, on: .main, in: .common)
-    .autoconnect()
+  @EnvironmentObject
+  private var controller: CircularProgressTimerController
   @State private(set) var width: CGFloat
   @State private(set) var height: CGFloat
   @State private(set) var borderWidth: CGFloat
@@ -17,11 +17,9 @@ public struct CircularProgressTimer: View {
   @State private(set) var trackColor: Color
   @State private(set) var barColor: Color
   @State private(set) var completionColor: Color
-  @State private(set) var min: Int
-  private(set) var max: Int
   @Binding private(set) var isCompleted: Bool
   
-  public init(min: Int, max: Int,
+  public init(
               width: CGFloat = 250, height: CGFloat = 250,
               borderWidth: CGFloat = 5.0,
               font: Font = .system(size: 20), fontColor: Color = .black,
@@ -30,8 +28,6 @@ public struct CircularProgressTimer: View {
               isCompleted: Binding<Bool> = .constant(false)) {
     self.width = width
     self.height = height
-    self.min = min
-    self.max = max
     self.borderWidth = borderWidth
     self.font = font
     self.fontColor = fontColor
@@ -50,15 +46,13 @@ public struct CircularProgressTimer: View {
         ProgressBar(width: width, height: height,
                     borderWidth: borderWidth,
                     barColor: barColor, completionColor: completionColor,
-                    counter: min, countTo: max)
-        Clock(font: font, fontColor: fontColor, counter: min, countTo: max)
+                    counter: controller.counter, countTo: controller.countTo)
+        Clock(font: font, fontColor: fontColor,
+              counter: controller.counter, countTo: controller.countTo)
       }
     }
-    .onReceive(timer) { time in
-      if (self.min < self.max) {
-        self.min += 1
-      }
-      if self.min == self.max {
+    .onReceive(controller.$counter) { newCounter in
+      if newCounter == controller.countTo {
         self.isCompleted = true
       }
     }
@@ -66,7 +60,7 @@ public struct CircularProgressTimer: View {
   
   @ViewBuilder
   public func borderWidth(_ width: CGFloat) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
+    CircularProgressTimer(
                      width: self.width, height: self.height,
                      borderWidth: width,
                      font: self.font, fontColor: self.fontColor,
@@ -83,75 +77,75 @@ public struct CircularProgressTimer: View {
       newWidth = Swift.max(newWidth, newHeight)
       newHeight = Swift.max(newWidth, newHeight)
     }
-    return CircularProgressTimer(min: self.min, max: self.max,
-                            width: newWidth, height: newHeight,
-                            borderWidth: self.borderWidth,
-                            font: self.font, fontColor: self.fontColor,
-                            trackColor: self.trackColor, barColor: self.barColor,
-                            completionColor: self.completionColor,
-                            isCompleted: self.$isCompleted)
+    return CircularProgressTimer(
+                                 width: newWidth, height: newHeight,
+                                 borderWidth: self.borderWidth,
+                                 font: self.font, fontColor: self.fontColor,
+                                 trackColor: self.trackColor, barColor: self.barColor,
+                                 completionColor: self.completionColor,
+                                 isCompleted: self.$isCompleted)
   }
   
   @ViewBuilder
   public func fontColor(_ color: Color) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
-                     width: self.width, height: self.height,
-                     borderWidth: self.borderWidth,
-                     font: self.font, fontColor: color,
-                     trackColor: self.trackColor, barColor: self.barColor,
-                     completionColor: self.completionColor,
-                     isCompleted: self.$isCompleted)
+    CircularProgressTimer(
+                          width: self.width, height: self.height,
+                          borderWidth: self.borderWidth,
+                          font: self.font, fontColor: color,
+                          trackColor: self.trackColor, barColor: self.barColor,
+                          completionColor: self.completionColor,
+                          isCompleted: self.$isCompleted)
   }
   
   @ViewBuilder
   public func trackColor(_ color: Color) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
-                     width: self.width, height: self.height,
-                     borderWidth: self.borderWidth,
-                     font: self.font, fontColor: self.fontColor,
-                     trackColor: color, barColor: self.barColor,
-                     completionColor: self.completionColor,
-                     isCompleted: self.$isCompleted)
+    CircularProgressTimer(
+                          width: self.width, height: self.height,
+                          borderWidth: self.borderWidth,
+                          font: self.font, fontColor: self.fontColor,
+                          trackColor: color, barColor: self.barColor,
+                          completionColor: self.completionColor,
+                          isCompleted: self.$isCompleted)
   }
   
   @ViewBuilder
   public func barColor(_ color: Color) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
-                     width: self.width, height: self.height,
-                     borderWidth: self.borderWidth,
-                     font: self.font, fontColor: self.fontColor,
-                     trackColor: self.trackColor, barColor: color,
-                     completionColor: self.completionColor,
-                     isCompleted: self.$isCompleted)
+    CircularProgressTimer(
+                          width: self.width, height: self.height,
+                          borderWidth: self.borderWidth,
+                          font: self.font, fontColor: self.fontColor,
+                          trackColor: self.trackColor, barColor: color,
+                          completionColor: self.completionColor,
+                          isCompleted: self.$isCompleted)
   }
   
   @ViewBuilder
   public func completionColor(_ color: Color) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
-                     width: self.width, height: self.height,
-                     borderWidth: self.borderWidth,
-                     font: self.font, fontColor: self.fontColor,
-                     trackColor: self.trackColor, barColor: self.barColor,
-                     completionColor: color,
-                     isCompleted: self.$isCompleted)
+    CircularProgressTimer(
+                          width: self.width, height: self.height,
+                          borderWidth: self.borderWidth,
+                          font: self.font, fontColor: self.fontColor,
+                          trackColor: self.trackColor, barColor: self.barColor,
+                          completionColor: color,
+                          isCompleted: self.$isCompleted)
   }
   
   @ViewBuilder
   public func clockFont(_ newFont: Font) -> CircularProgressTimer {
-    CircularProgressTimer(min: self.min, max: self.max,
-                     width: self.width, height: self.height,
-                     borderWidth: self.borderWidth,
-                     font: newFont, fontColor: self.fontColor,
-                     trackColor: self.trackColor, barColor: self.barColor,
-                     completionColor: self.completionColor,
-                     isCompleted: self.$isCompleted)
+    CircularProgressTimer(
+                          width: self.width, height: self.height,
+                          borderWidth: self.borderWidth,
+                          font: newFont, fontColor: self.fontColor,
+                          trackColor: self.trackColor, barColor: self.barColor,
+                          completionColor: self.completionColor,
+                          isCompleted: self.$isCompleted)
   }
 }
 
 @available(iOS 13.0.0, macOS 10.15, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 struct CircularProgressTimer_Previews: PreviewProvider {
   static var previews: some View {
-    CircularProgressTimer(min: 0, max: 80)
+    CircularProgressTimer()
       .clockSize(CGSizeMake(300, 300))
       .borderWidth(18)
       .fontColor(.black)
@@ -159,5 +153,7 @@ struct CircularProgressTimer_Previews: PreviewProvider {
       .barColor(.orange)
       .completionColor(.green)
       .clockFont(.system(size: 70))
+      .environmentObject(CircularProgressTimerController(timer: Timer
+        .publish(every: 1, on: .main, in: .common)))
   }
 }
---
+++ Sources/CircularProgressTimer/CircularProgressTimerController.swift
@@ -0,0 +1,75 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sean Li on 2022/12/21.
+//
+
+import Foundation
+import Combine
+
+public protocol CircularProgressTimerControllable
+where Self: ObservableObject {
+  var timer: Timer.TimerPublisher { get }
+  var counter: Int { get }
+  var countTo: Int { get }
+  func resume()
+  func pause()
+  func reset()
+  func cancel()
+}
+
+public class CircularProgressTimerController: ObservableObject, CircularProgressTimerControllable
+{
+  private var timerSubscription: AnyCancellable?
+  public let timer: Timer.TimerPublisher
+  let initialCounter: Int
+  @Published public private(set) var counter: Int
+  @Published private var paused: Bool = true
+  public let countTo: Int
+  
+  init(timer: Timer.TimerPublisher,
+       counter: Int = 0, countTo: Int = 60) {
+    self.timer = timer
+    self.initialCounter = counter
+    self.counter = counter
+    self.countTo = countTo
+    setupSubscriptions()
+  }
+  
+  deinit {
+    timerSubscription?.cancel()
+    timerSubscription = nil
+  }
+  
+  private func setupSubscriptions() {
+    timerSubscription = timer
+      .autoconnect()
+      .combineLatest($paused)
+      .sink(receiveValue: { [unowned self] (_, isPaused) in
+        guard counter < countTo else {
+          return
+        }
+        if !isPaused { counter += 1 }
+      })
+  }
+  
+  public func resume() {
+    self.paused = false
+  }
+  
+  public func pause() {
+    self.paused = true
+  }
+  
+  public func reset() {
+    pause()
+    counter = initialCounter
+  }
+  
+  public func cancel() {
+    pause()
+    timerSubscription?.cancel()
+    timerSubscription = nil
+  }
+}
